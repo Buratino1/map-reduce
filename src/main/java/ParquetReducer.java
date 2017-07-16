533525ec45c01ca6ef09c5d010cd3ffeeb97cc3d
DG <gigant2006@gmail.com>
2017-07-17 00:04:42 +0300

    reducer tests

--- src/main/java/ParquetReducer.java
+++ src/main/java/ParquetReducer.java
@@ -10,17 +10,22 @@ public class ParquetReducer extends Reducer<Text, AvroValue<GenericRecord>, Void
 
     @Override
     protected void reduce(Text key, Iterable<AvroValue<GenericRecord>> values, Context context) throws IOException, InterruptedException {
+/*
+        Queue<Integer> queue = new LinkedList<Integer>();
+        List<Integer> rows = new ArrayList<Integer>();
+        AvroValue<GenericRecord> rn ;
 
-        Queue<String> queue = new LinkedList<String>();
         Integer prevValue = 0;
 
         for (AvroValue<GenericRecord> value : values) {
-            if (value.datum().get("type") == "original") {
-                queue.add(value.datum().get("value").toString()) ;
-
-            } else {
-                prevValue = Integer.parseInt(queue.remove());
-            }
+            Integer nId  = (Integer) value.datum().get("id") ;
+            Integer nVal = (Integer) value.datum().get("value") ;
+            String sType = (String) value.datum().get("type") ;
+            rows.add(nId, nVal) ;
+            rn = value ;
+        }
+*/
+        for (AvroValue<GenericRecord> value : values) {
             context.write(null, value.datum());
         }
     }
