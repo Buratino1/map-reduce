f36fd7cf8c4313d188a4aeca816d876424efed76
DG <gigant2006@gmail.com>
2017-07-16 23:07:29 +0300

    queene added

--- src/main/java/ParquetReducer.java
+++ src/main/java/ParquetReducer.java
@@ -4,19 +4,23 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.*;
 
 public class ParquetReducer extends Reducer<Text, AvroValue<GenericRecord>, Void, GenericRecord> {
 
-    /**
-     * Does nothing but pass the values through.  Do some more processing here.
-     *
-     * @param key     the reducer's key
-     * @param values  all of this key's records as an Avro representation of the Parquet record
-     * @param context the reducer's context
-     */
     @Override
     protected void reduce(Text key, Iterable<AvroValue<GenericRecord>> values, Context context) throws IOException, InterruptedException {
+
+        Queue<String> queue = new LinkedList<String>();
+        Integer prevValue = 0;
+
         for (AvroValue<GenericRecord> value : values) {
+            if (value.datum().get("type") == "original") {
+                queue.add(value.datum().get("value").toString()) ;
+
+            } else {
+                prevValue = Integer.parseInt(queue.remove());
+            }
             context.write(null, value.datum());
         }
     }
