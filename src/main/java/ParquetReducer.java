d9d0bfd848605cb1ba635eae617eb3dac7ac37a7
DG <gigant2006@gmail.com>
2017-07-17 23:02:28 +0300

    last value column

--- src/main/java/ParquetReducer.java
+++ src/main/java/ParquetReducer.java
@@ -1,30 +1,33 @@
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.mapred.AvroValue;
-import org.apache.avro.mapred.Pair;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
-
 import java.io.IOException;
 import java.util.*;
 
 public class ParquetReducer extends Reducer<Text, AvroValue<GenericRecord>, Void, Text> {
-    private TreeMap<Integer, Pair<String, Integer>> rows = new TreeMap<Integer, Pair<String, Integer>>();
+    private TreeMap<Integer, AbstractMap.SimpleEntry<String, Integer>> rows = new TreeMap<Integer,AbstractMap.SimpleEntry<String, Integer>>();
+    private String adj = "";
+    private Integer lastValue = -1;
 
     @Override
     protected void reduce(Text key, Iterable<AvroValue<GenericRecord>> values, Context context) throws IOException, InterruptedException {
-        // Map<Integer, Integer> rows = new TreeMap<Integer, Integer>();
-
-        GenericRecord record = null;
         for (AvroValue<GenericRecord> value : values) {
-            // Integer nId  = (Integer) value.datum().get("id") ;
-            rows.put((Integer) value.datum().get("id") , new Pair<String, Integer>(value.datum().get("type"), value.datum().get("value") ) ) ;
+            AbstractMap.SimpleEntry<String, Integer> pair = new AbstractMap.SimpleEntry(value.datum().get("type"), value.datum().get("value")) ;
+            rows.put((Integer) value.datum().get("id") , pair) ;
         }
 
-        for(Map.Entry<Integer, Pair<String, Integer>> entry : rows.entrySet()) {
-            Integer rowId = entry.getKey();
-            Pair<String, Integer> rowValue = entry.getValue();
-            Text output = new Text(rowId+" "+rowValue.toString());
+        for(Map.Entry<Integer, AbstractMap.SimpleEntry<String, Integer>> entry : rows.entrySet()) {
+            AbstractMap.SimpleEntry<String, Integer> rowValue = entry.getValue();
+
+            if (rowValue.getKey() == "original") {
+                lastValue = rowValue.getValue() ;
+                adj = "" ;
+            } else {
+                adj = " " + lastValue.toString();
+            }
+            Text output = new Text(entry.getKey()+" "+rowValue.getKey() + " " + rowValue.getValue() + adj);
+
             context.write(null, output );
         }
     }
